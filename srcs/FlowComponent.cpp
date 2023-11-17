a2c273034516b281057ad8b6e23b0e93eeebb0c0
WuChang <3142324836@qq.com>
2023-11-17 23:45:03 +0800

    Update FlowComponent

--- srcs/FlowComponent.cpp
+++ srcs/FlowComponent.cpp
@@ -1,6 +1,17 @@
 ﻿#include "FlowComponent.h"
+#include "FlowWindow.h"
 
 namespace flowUI {
 	FlowComponent::FlowComponent(const juce::String& name)
 		: juce::Component(name) {}
+
+	FlowWindow* FlowComponent::getWindow() const {
+		for (const juce::Component* currentComp = this;
+			currentComp != nullptr; currentComp = currentComp->getParentComponent()) {
+			if (auto ptr = dynamic_cast<const FlowWindow*>(currentComp)) {
+				return const_cast<FlowWindow*>(ptr);
+			}
+		}
+		return nullptr;
+	}
 }
--- srcs/FlowComponent.h
+++ srcs/FlowComponent.h
@@ -3,9 +3,14 @@
 #include <JuceHeader.h>
 
 namespace flowUI {
+	class FlowWindow;
+
 	class FlowComponent : public juce::Component {
 	public:
 		FlowComponent(const juce::String& name);
+
+	protected:
+		FlowWindow* getWindow() const;
 
 	private:
 		JUCE_DECLARE_NON_COPYABLE_WITH_LEAK_DETECTOR(FlowComponent)
